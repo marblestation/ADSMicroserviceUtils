2d2243f188650d614b151ca9f8724255c60b07e5
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2018-08-29 09:52:13 -0400

    Grab version from git tags and read requirements from file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build Status](https://travis-ci.org/adsabs/ADSMicroserviceUtils.svg?branch=master)](https://travis-ci.org/adsabs/ADSMicroserviceUtils)
 [![Coverage Status](https://coveralls.io/repos/github/adsabs/ADSMicroserviceUtils/badge.svg?branch=master)](https://coveralls.io/github/adsabs/ADSMicroserviceUtils?branch=master)
 
-# ADSPipelineUtils
+# ADSMicroserviceUtils
 Set of common libraries used by ADS microservices.
 
 Class ADSFlask is to be used by all the microservice applications, where logging is setup automatically. 
--- setup.py
+++ setup.py
@@ -1,21 +1,42 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 import os
+from subprocess import Popen, PIPE
 
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
-setup(name='adsmutils',
-      version='0.0.1',
-      packages=['adsmutils'],
-      install_requires=[
-          'ConcurrentLogHandler==0.9.1',
-          'python-dateutil==2.6.0',
-          'DateTime==4.1.1',
-          'SQLAlchemy==1.1.6',
-          'setuptools>=36.5.0',
-          'six>=1.11.0',
-          'Flask-SQLAlchemy==2.2',
-          'python-json-logger==0.1.8'
-      ],
-  )
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ""
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+def get_git_version(default="v0.0.1"):
+    try:
+        p = Popen(['git', 'describe', '--tags'], stdout=PIPE, stderr=PIPE)
+        p.stderr.close()
+        line = p.stdout.readlines()[0]
+        line = line.strip()
+        return line
+    except:
+        return default
+
+setup(
+    name='adsmutils',
+    version=get_git_version(default="v0.0.1"),
+    url='https://github.com/adsabs/ADSMicroserviceUtils',
+    license='MIT',
+    author="NASA/SAO ADS",
+    description='ADS Microservice Utils',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=required,
+)
+
