b1b54a705c24376136ca4b9ac0b14e713692fcb1
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2018-08-29 14:19:53 -0400

    Added readiness and liveliness endpoints for all microservices

---
+++ adsmutils/tests/test_service.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from base import TestCase, TestCaseDatabase
+import mock
+
+class TestServices(TestCase):
+
+    def test_readiness_probe(self):
+        '''Tests for the existence of a /ready route, and that it returns properly
+        formatted JSON data'''
+        r = self.client.get('/ready')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json['ready'], True)
+
+    def test_liveliness_probe(self):
+        '''Tests for the existence of a /alive route, and that it returns properly
+        formatted JSON data'''
+        r = self.client.get('/alive')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json['alive'], True)
+
+
+class TestServicesWithDatabase(TestCaseDatabase):
+
+    def test_readiness_probe(self):
+        '''Tests for the existence of a /ready route, and that it returns properly
+        formatted JSON data'''
+        r = self.client.get('/ready')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json['ready'], True)
+
+    def test_liveliness_probe(self):
+        '''Tests for the existence of a /alive route, and that it returns properly
+        formatted JSON data'''
+        r = self.client.get('/alive')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json['alive'], True)
+
+    def test_readiness_probe_with_db_failure(self):
+        '''Tests for the existence of a /ready route, and that it returns properly
+        formatted JSON data when database connection has been lost'''
+        self.app._db_failure = mock.MagicMock(return_value=True)
+        r = self.client.get('/ready')
+        self.assertEqual(r.status_code, 503)
+        self.assertEqual(r.json['ready'], False)
+
+    def test_liveliness_probe_with_db_failure(self):
+        '''Tests for the existence of a /alive route, and that it returns properly
+        formatted JSON data when database connection has been lost'''
+        self.app._db_failure = mock.MagicMock(return_value=True)
+        r = self.client.get('/alive')
+        self.assertEqual(r.status_code, 503)
+        self.assertEqual(r.json['alive'], False)
